f9e635dcce3e1de27090d08e980517c7df7625d0
yusufbell <yusufb@umich.edu>
2016-11-10 16:51:04 -0500

    Almost finished part B

--- HW3-StudentCopy/bshw3.py
+++ HW3-StudentCopy/bshw3.py
@@ -10,3 +10,22 @@
 
 # Deliverables
 # Make sure the new page is uploaded to your GitHub account.
+
+import requests
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+
+base_url = 'http://collemc.people.si.umich.edu/data/bshw3StarterFile.html'
+html = urllib.request.urlopen(base_url)
+#r = requests.get(base_url)
+soup = BeautifulSoup(html, "html.parser")
+html_doc = soup.prettify()
+html_doc = html_doc.replace('student', 'AMAZING student')
+html_doc = html_doc.replace('https://testbed.files.wordpress.com/2012/09/bsi_exposition_041316_', 'media/logo.png')
+html_doc = html_doc.replace('logo2.png', 'media/logo.png')
+fout = open('yusufb.html', 'w')
+fout.write(html_doc)
+fout.close()
+
+
+
