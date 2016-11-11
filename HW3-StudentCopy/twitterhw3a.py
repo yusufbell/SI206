b950679d0d68f0a398f3c793bf24d66df33cb2b0
yusufbell <yusufb@umich.edu>
2016-11-11 14:15:28 -0500

    Finished Twitter Part A

--- HW3-StudentCopy/twitterhw3a.py
+++ HW3-StudentCopy/twitterhw3a.py
@@ -4,5 +4,20 @@
 
 # You will demo this live for grading.
 
-print("""No output necessary although you 
-	can print out a success/failure message if you want to.""")
+import tweepy
+import nltk
+
+# Unique code from Twitter
+access_token = "783672422-f8n5mxjJdPinhDJNpoj4EPeMmyIbf5TQqVUjEtjz"
+access_token_secret = "VQHV0tDXRRaaYkW2nq9ZXTbabnEHlXESKJn79P9itBwnd"
+consumer_key = "cCxQmwhltBPqkV8njvc9xaZ94"
+consumer_secret = "TAjrbPAm2ElfJzfpDWuXGHAzzGI1kpsbGUkn8vcjzke3dw89KP"
+
+# Boilerplate code here
+auth = tweepy.OAuthHandler(consumer_key,consumer_secret)
+auth.set_access_token(access_token,access_token_secret)
+
+api = tweepy.API(auth)
+api.update_with_media('media/yusuf.jpg', status= '#UMSI-206 #Proj3')
+
+
